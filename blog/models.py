2210b73f0940592ed299fbcd677c762e3321f5de
Miguel <mig.estrada@alumno.duoc.cl>
2019-06-07 19:03:09 -0400

    Agrega modelo de sitio

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Lugar(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(max_length=1000)
+    ubicación = models.CharField(max_length=1000)
+    imagen = models.ImageField()
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'inicio.html', {})
+
+def sitio_registro(request):
+    return render(request, 'sitio/registro.html', {})
